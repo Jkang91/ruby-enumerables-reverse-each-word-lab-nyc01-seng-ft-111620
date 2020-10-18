24ac0d1ec3b1b0016b6f94d6f3340b179f530e46
June Young Kang <jkang34@gmail.com>
2020-10-18 19:48:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
-def reverse_each_word(sentence)
- new_sent = []
- sentence.split.each do |word|
-   word = word.reverse 
-   new_sent << word 
-  end 
-  new_sent.join(" ")
-end 
+# def reverse_each_word(sentence)
+# new_sent = []
+# sentence.split.each do |word|
+#   word = word.reverse 
+#   new_sent << word 
+#   end 
+#   new_sent.join(" ")
+# end 
+
